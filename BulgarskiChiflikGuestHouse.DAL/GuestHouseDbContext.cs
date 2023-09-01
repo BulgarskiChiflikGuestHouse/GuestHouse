ec2abbe1277f663fff47cc3e4b0c6b9e00be1638
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-09-02 02:19:03 +0300

    setup: #50 mssql architecture
    setup: #50 create mssql database architecture

---
+++ BulgarskiChiflikGuestHouse.DAL/GuestHouseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuestHouse.DAL
+{
+	public class GuestHouseDbContext : DbContext
+	{
+        public GuestHouseDbContext(DbContextOptions<GuestHouseDbContext> options)
+			: base(options)
+        {
+			
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
---
+++ BulgarskiChiflikGuestHouse.Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using GuestHouse.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GuestHouse.Infrastructure
+{
+	public static class ApplicationBuilderExtensions
+	{
+		public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app) 
+		{
+			var serviceScope = app.ApplicationServices.CreateScope();
+			var service = serviceScope.ServiceProvider;
+
+			MigrateDatabase(service);
+
+			return app;
+		}
+
+		private static void MigrateDatabase(IServiceProvider service)
+		{
+			var data = service.GetRequiredService<GuestHouseDbContext>();
+			data.Database.Migrate();
+		}
+	}
+}
