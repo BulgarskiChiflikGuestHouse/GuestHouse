3528ad8d0002f791a92843c14961cfdac16c195f
TihomirAleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-09-13 13:12:20 +0300

    feature (user): #9 add rooms page content

---
+++ BulgarskiChiflikGuestHouse.Web/Controllers/RooomsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuestHouse.Web.Controllers
+{
+    public class RooomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
